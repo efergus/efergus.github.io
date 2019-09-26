7d0a2c4ed0e69342580ca9927d737e4f60b2fc0a
Ethan Ferguson <ethanmferguson@gmail.com>
2019-09-25 20:09:48 -0400

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+function resize() {
+    h = document.getElementById("description_text").offsetHeight;
+    document.getElementById("me_img").style.height = "${h}px";
+}
---
+++ scripts/sizing.js
@@ -0,0 +1,4 @@
+export function resize() {
+    h = document.getElementById("description_text").height;
+    console.log(4);
+}
