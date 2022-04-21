84b4b4e90a6fd17d11a6aaccc457d73864ce6ad7
Ethan Ferguson <sometimescode@gmail.com>
2022-04-21 14:11:42 -0400

    Slightly nicer

--- projects/title/main.js
+++ projects/title/main.js
@@ -1,19 +1,19 @@
 
 const G = document.getElementById.bind(document);
+const Q = document.querySelectorAll.bind(document);
+const C = document.createElement.bind(document);
+
+const colors = 'aqua, black, blue, fuchsia, gray, green, lime, maroon, navy, purple, red, silver, teal, white, yellow'.split(', ');
 
 function change_title() {
     let new_title = G("input").value;
     document.title = new_title;
     console.log('Title changed to "' + new_title + '"');
+    document.activeElement.blur();
     return false;
 }
 
-function main() {
-    G("form").onsubmit = change_title;
-    changeFavicon('red', 'red');
-}
-
-function changeFavicon(fill, stroke) {
+function create_icon(fill, stroke) {
     const canvas = document.createElement('canvas');
     canvas.height = 64;
     canvas.width = 64;
@@ -28,14 +28,37 @@ function changeFavicon(fill, stroke) {
     context.lineWidth = 5;
     context.strokeStyle = stroke;
     context.stroke();
+    return canvas;
+}
 
-    const link = document.createElement('link');
-    const oldLinks = document.querySelectorAll('link[rel="shortcut icon"]');
+function create_options(div) {
+    console.log(colors);
+    for(let i = 0; i < colors.length; i++) {
+        let canvas = create_icon(colors[i], colors[i]);
+        canvas.addEventListener("click", () => {change_favicon(colors[i], colors[i]);});
+        div.appendChild(canvas);
+    }
+}
+
+function main() {
+    G("form").onsubmit = change_title;
+    change_favicon('red', 'red');
+    create_options(G("options"));
+}
+
+function set_icon_to_canvas(canvas) {
+    const link = C('link');
+    const oldLinks = Q('link[rel="shortcut icon"]');
     oldLinks.forEach(e => e.parentNode.removeChild(e));
     link.id = 'dynamic-favicon';
     link.rel = 'shortcut icon';
     link.href = canvas.toDataURL();
     document.head.appendChild(link);
+}
+
+function change_favicon(fill, stroke) {
+    let canvas = create_icon(fill, stroke);
+    set_icon_to_canvas(canvas);
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
