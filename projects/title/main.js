e0f95e13eda85be40d6f55796ed1632ce57aff12
Ethan Ferguson <sometimescode@gmail.com>
2022-04-21 17:26:41 -0400

    Set title, nicer colors, pretty animations

--- projects/title/main.js
+++ projects/title/main.js
@@ -3,47 +3,64 @@ const G = document.getElementById.bind(document);
 const Q = document.querySelectorAll.bind(document);
 const C = document.createElement.bind(document);
 
-const colors = 'aqua, black, blue, fuchsia, gray, green, lime, maroon, navy, purple, red, silver, teal, white, yellow'.split(', ');
+const colors = '#BF360C #E53935 #F57F17 #FBC02D #D81B60 #8E24AA #5E35B1 #3949AB #1E88E5 #0D47A1 #00897B #1B5E20 #607D8B #757575'.split(' ');
+
+let bg_color = '#E53935';
+let bd_color = '#E53935';
 
 function change_title() {
-    let new_title = G("input").value;
+    let input = G("input");
+    let new_title = input.value;
     document.title = new_title;
     console.log('Title changed to "' + new_title + '"');
-    document.activeElement.blur();
+    input.placeholder = new_title;
+    input.value = "";
+    input.blur();
     return false;
 }
 
+function create_dot(fill, stroke) {
+    const elem = C('span');
+    elem.style.backgroundColor = fill;
+    elem.style.borderColor = stroke;
+    elem.classList.add('dot');
+    return elem;
+}
+
 function create_icon(fill, stroke) {
-    const canvas = document.createElement('canvas');
+    const canvas = C('canvas');
     canvas.height = 64;
     canvas.width = 64;
     const context = canvas.getContext('2d');
     const centerX = canvas.width / 2;
     const centerY = canvas.height / 2;
-    const radius = 30;
+    const radius = 24;
     context.beginPath();
     context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
     context.fillStyle = fill;
     context.fill();
-    context.lineWidth = 5;
+    context.lineWidth = 12;
     context.strokeStyle = stroke;
     context.stroke();
     return canvas;
 }
 
-function create_options(div) {
+function create_options(options) {
     console.log(colors);
+    let div = C("div");
     for(let i = 0; i < colors.length; i++) {
-        let canvas = create_icon(colors[i], colors[i]);
-        canvas.addEventListener("click", () => {change_favicon(colors[i], colors[i]);});
-        div.appendChild(canvas);
+        let elem = create_dot(colors[i], colors[i]);
+        elem.addEventListener("click", () => {change_favicon(colors[i], undefined);});
+        div.appendChild(elem);
     }
-}
-
-function main() {
-    G("form").onsubmit = change_title;
-    change_favicon('red', 'red');
-    create_options(G("options"));
+    options.appendChild(div);
+    let border = C("div");
+    for(let i = 0; i < colors.length; i++) {
+        let elem = create_dot("transparent", colors[i]);
+        elem.addEventListener("click", () => {change_favicon(bg_color, colors[i]);});
+        border.appendChild(elem);
+    }
+    options.appendChild(border);
 }
 
 function set_icon_to_canvas(canvas) {
@@ -57,8 +74,19 @@ function set_icon_to_canvas(canvas) {
 }
 
 function change_favicon(fill, stroke) {
+    if (stroke === undefined) {
+        stroke = fill;
+    }
+    bg_color = fill;
+    bd_color = stroke;
     let canvas = create_icon(fill, stroke);
     set_icon_to_canvas(canvas);
+}
+
+function main() {
+    G("form").onsubmit = change_title;
+    change_favicon(bg_color, bd_color);
+    create_options(G("options"));
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
